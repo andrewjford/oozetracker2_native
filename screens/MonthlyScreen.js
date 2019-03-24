b276163b2cb8aee533b862bef5b0f92250c4c261
AJ <ajoeford@gmail.com>
2019-03-24 11:15:06 -0700

    monthly screen shows data properly

--- screens/MonthlyScreen.js
+++ screens/MonthlyScreen.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {
-  Image,
-  Platform,
   ScrollView,
   StyleSheet,
   Text,
   View,
-  Button
 } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { getMonthly } from '../actions/expenseActions';
+
+const TableRow = (props) => {
+  return (
+    <View style={styles.tableRow}>
+      <View style={styles.tableCell}><Text>{props.lineItem.name}</Text></View>
+      <View style={styles.tableCell}>
+        <Text style={styles.textRight}>{props.lineItem.sum}</Text>
+      </View>
+    </View>
+  );
+}
 
 class MonthlyScreen extends React.Component {
   constructor(props) {
@@ -34,13 +42,10 @@ class MonthlyScreen extends React.Component {
   }
 
   renderLineItems = () => {
-    if (!this.props.monthlyView) {return <Text>Loading</Text>};
+    if (!this.props.monthlyView) {return <Text>No Recorded Expenses</Text>};
     return this.props.monthlyView.rows.map((lineItem) => {
       return (
-        <View key={lineItem.id}>
-          <View><Text>{lineItem.name}</Text></View>
-          <View align="right"><Text>{lineItem.sum}</Text></View>
-        </View>
+        <TableRow lineItem={lineItem} key={lineItem.id}/>
       );
     });
   }
@@ -52,9 +57,14 @@ class MonthlyScreen extends React.Component {
     
     return (
       <View style={styles.container}>
+      <Text style={styles.headerText}>
+        {this.state.date.getFullYear()} {this.state.monthNames[this.state.date.getMonth()]}
+      </Text>
+
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
+          <View style={styles.tableContainer}>
             {this.renderLineItems()}
+            <TableRow lineItem={{name: "Total", sum: total}} />
           </View>
         </ScrollView>
 
@@ -83,87 +93,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+    alignSelf: 'center',
+    width: '75%',
   },
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
+  tableContainer: {
+    flex: 1,
     marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  tableRow: {
+    flex: 1,
+    alignSelf: 'stretch',
+    flexDirection: 'row',
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  tableCell: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  textRight: {
+    textAlign: 'right',
   },
   homeScreenFilename: {
     marginVertical: 7,
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
+  headerText: {
+    fontSize: 20,
     textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+    margin: 5,
+    fontWeight: 'bold',
   },
 });
