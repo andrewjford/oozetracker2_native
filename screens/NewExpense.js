749c567fdd678f3fafa717269a7d9334d027f2ea
AJ <ajoeford@gmail.com>
2019-07-14 11:44:22 -0700

    update error styling on new expense screen, refactor component

--- screens/NewExpense.js
+++ screens/NewExpense.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { View, StyleSheet, ActivityIndicator, ScrollView, Picker } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import currency from "currency.js";
 
-import ExpenseInput from "../components/ExpenseInput";
+import DatePicker from "../components/DatePicker";
 import { createExpense } from "../actions/expenseActions";
 import Colors from "../constants/Colors";
 import ErrorDisplay from "../components/ErrorDisplay";
@@ -12,9 +14,15 @@ import ErrorHandling from "../services/ErrorHandling";
 class NewExpense extends React.Component {
   constructor(props) {
     super(props);
+    const theDate = new Date();
+
     this.state = {
       isLoading: false,
-      errors: []
+      errors: [],
+      description: "",
+      amount: currency(0).format(),
+      date: theDate,
+      category: this.props.categories[0].id
     };
   }
 
@@ -47,6 +55,33 @@ class NewExpense extends React.Component {
     this.setState({ errors: [] });
   };
 
+  categories = this.props.categories.map(category => {
+    return (
+      <Picker.Item
+        label={category.name}
+        value={category.id}
+        key={category.id}
+      />
+    );
+  });
+
+  handleAmountChange = text => {
+    this.setState({ amount: text });
+  };
+
+  handleCategoryChange = newcategory => {
+    this.setState({ category: newcategory });
+  };
+
+  handleSubmit = () => {
+    this.addExpense({
+      description: this.state.description,
+      amount: this.state.amount,
+      date: this.state.date,
+      category: this.state.category
+    });
+  }
+
   render() {
     if (this.state.isLoading) {
       return (
@@ -57,19 +92,54 @@ class NewExpense extends React.Component {
         />
       );
     }
-    // need to fix height of error display
     return (
       <View style={styles.container}>
-        <ErrorDisplay
-          style={styles.error}
-          errors={this.state.errors}
-          clearErrors={this.clearErrors}
-        />
-        <ExpenseInput
-          title={"Create Expense"}
-          categories={this.props.categories}
-          createExpense={this.addExpense}
-        />
+        <ScrollView style={styles.container}>
+          <ErrorDisplay
+            errors={this.state.errors}
+            clearErrors={this.clearErrors}
+          />
+          <View style={styles.inputFields}>
+            <DatePicker
+              style={styles.inputContainerStyle}
+              date={this.state.date}
+              onDateChange={date => this.setState({ date })}
+            />
+            <TextInput
+              style={styles.inputContainerStyle}
+              label="Description"
+              value={this.state.description}
+              selectTextOnFocus={true}
+              onChangeText={description => this.setState({ description })}
+              underlineColor={Colors.accentColor}
+              selectionColor={Colors.secondaryColor}
+            />
+
+            <TextInput
+              style={styles.inputContainerStyle}
+              label="Amount"
+              keyboardType="numeric"
+              value={String(this.state.amount)}
+              selectTextOnFocus={true}
+              onChangeText={this.handleAmountChange}
+              underlineColor={Colors.accentColor}
+              selectionColor={Colors.secondaryColor}
+            />
+
+            <Picker
+              selectedValue={this.state.category}
+              style={styles.inputContainerStyle}
+              onValueChange={this.handleCategoryChange}
+            >
+              {this.categories}
+            </Picker>
+          </View>
+        </ScrollView>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.handleSubmit} mode="contained" style={styles.button}>
+            Submit
+          </Button>
+        </View>
       </View>
     );
   }
@@ -101,7 +171,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  error: {
-    height: 32
+  inputFields: {
+    padding: 16
+  },
+  inputContainerStyle: {
+    margin: 8
+  },
+  button: {
+    width: "40%",
+    alignSelf: "center"
+  },
+  buttonContainer: {
+    height: "10%",
+    marginVertical: 8,
+    borderTopColor: Colors.accentColor,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    justifyContent: "center"
   }
 });
